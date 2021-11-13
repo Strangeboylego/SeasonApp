74b768cc325a69dcf70adb95e876c298f8a66ad0
Strangeboylego <halushkooleh@gmail.com>
2021-11-13 14:44:50 +0200

    Init Commit

---
+++ src/main/java/com/example/oleh/seasonapp/SeasonAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.oleh.seasonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasonAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeasonAppApplication.class, args);
+    }
+
+}
