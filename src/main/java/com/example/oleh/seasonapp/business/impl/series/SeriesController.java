b8e4410d8c8e96313116d861108c000f4280ca36
Strangeboylego <halushkooleh@gmail.com>
2021-11-17 09:22:29 +0200

    optimize imports

--- src/main/java/com/example/oleh/seasonapp/business/impl/series/SeriesController.java
+++ src/main/java/com/example/oleh/seasonapp/business/impl/series/SeriesController.java
@@ -2,7 +2,6 @@ package com.example.oleh.seasonapp.business.impl.series;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
